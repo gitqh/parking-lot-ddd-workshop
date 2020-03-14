3d1f1655e5f31c39ac42ffba784b712b4f5f0006
gitqh <quhang19881208@163.com>
2020-03-14 11:29:33 +0800

    Add arch unit for architecture guarding

---
+++ src/test/java/com/thoughtworks/parkinglot/architecture/HexagonalArchitectureTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.parkinglot.architecture;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author gitqh
+ */
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "com.thoughtworks.parkinglot")
+public class HexagonalArchitectureTest {
+
+
+    @ArchTest
+    static final ArchRule hexagonal_architecture_is_respected = onionArchitecture()
+            .domainModels("..domain.model..")
+            .domainServices("..domain.service..")
+            .applicationServices("..application..")
+            .adapter("api", "..adapter.api..")
+            .adapter("repository", "..adapter.repository..");
+
+}
